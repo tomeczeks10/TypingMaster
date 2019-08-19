7e7ed3234c4be588f9fd7b2630fb8e2fa5e7f6bb
tomeczeks10 <tomeczeks10@gazeta.pl>
2019-08-19 23:04:40 +0200

    Data is working

--- app/src/main/java/com/szrl/tomasz/typingmaster/StartTestActivity.java
+++ app/src/main/java/com/szrl/tomasz/typingmaster/StartTestActivity.java
@@ -11,6 +11,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class StartTestActivity extends AppCompatActivity {
@@ -22,18 +23,21 @@ public class StartTestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starttest);
         loadData = (TextView) findViewById(R.id.input_textview);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("keyboard-f2b83/Words");
+        DatabaseReference ref = database.getReferenceFromUrl("https://keyboard-f2b83.firebaseio.com/Words");
 
 // Attach a listener to read the data at our posts reference
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String s = dataSnapshot.getValue(String.class);
-                System.out.println("tomek"+s);
-                Log.i("msg",s);
-                //loadData.setText(w.word);
-            }
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String w = ds.child("word").getValue(String.class);
 
+                    // Words w = dataSnapshot.getValue(Words.class);
+                    System.out.println("tomek" + w);
+                    //  Log.i("msg",w.getWord());
+                    loadData.setText(w);
+                }
+            }
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 System.out.println("The read failed: " + databaseError.getCode());
@@ -41,4 +45,5 @@ public class StartTestActivity extends AppCompatActivity {
         });
 
     }
+
 }
